28cae6ca2120da3fd6e7662215ade756e900b69c
Zachary Guns <zacharyguns@yahoo.com>
2018-07-10 21:58:19 -0700

    minor additions

---
+++ src/components/Game/Game.js
@@ -0,0 +1,71 @@
+import React, { Component } from "react";
+import "./Game.css";
+
+class Game extends Component {
+  // Setting the component's initial state
+  state = {
+    firstName: "",
+    lastName: "",
+    password: ""
+  };
+
+  handleInputChange = event => {
+    // Getting the value and name of the input which triggered the change
+    const value = event.target.value;
+    const name = event.target.name;
+
+    // Updating the input's state
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleGameSubmit = event => {
+    // Preventing the default behavior of the Game submit (which is to refresh the page)
+    event.preventDefault();
+
+    // Alert the user their first and last name, clear `this.state.firstName` and `this.state.lastName`, clearing the inputs
+    alert(`Hello ${this.state.firstName} ${this.state.lastName}`);
+    this.setState({
+      firstName: "",
+      lastName: ""
+    });
+  };
+
+  render() {
+    // Notice how each input has a `value`, `name`, and `onChange` prop
+    return (
+      <div>
+        <p>
+          Hello {this.state.firstName} {this.state.lastName}
+        </p>
+        <Game className="Game">
+          <input
+            value={this.state.firstName}
+            name="firstName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="First Name"
+          />
+          <input
+            value={this.state.lastName}
+            name="lastName"
+            onChange={this.handleInputChange}
+            type="text"
+            placeholder="Last Name"
+          />
+          <input
+            value={this.state.password}
+            name="password"
+            onChange={this.handleInputChange}
+            type="password"
+            placeholder="Password"
+          />
+          <button onClick={this.handleGameSubmit}>Submit</button>
+        </Game>
+      </div>
+    );
+  }
+}
+
+export default Game;
