014690a9bb8e94301806980586731191aa82f4be
Zachary Guns <zacharyguns@yahoo.com>
2018-07-11 21:58:02 -0700

    very slow progress

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -12,36 +12,6 @@ const NavTabs = props => (
         Home
       </a>
     </li>
-    <li className="nav-item">
-      <a
-        onClick={() => props.handlePageChange("About")}
-        className={
-          props.currentPage === "About" ? "nav-link active" : "nav-link"
-        }
-      >
-        About
-      </a>
-    </li>
-    <li className="nav-item">
-      <a
-        onClick={() => props.handlePageChange("Blog")}
-        className={
-          props.currentPage === "Blog" ? "nav-link active" : "nav-link"
-        }
-      >
-        Blog
-      </a>
-    </li>
-    <li className="nav-item">
-      <a
-        onClick={() => props.handlePageChange("Contact")}
-        className={
-          props.currentPage === "Contact" ? "nav-link active" : "nav-link"
-        }
-      >
-        Contact
-      </a>
-    </li>
   </ul>
 );
 
